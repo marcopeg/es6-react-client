3c3de67b5168996c73b98142f5eda58804e26c9d
Marco Pegoraro <marco.pegoraro@gmail.com>
2015-12-19 09:51:46 +0100

    add tests for App container #2

--- app/app.dev.js
+++ app/app.dev.js
@@ -11,7 +11,7 @@ import ReactDOM from 'react-dom';
 import { Main } from 'utils/main-dev';
 import { makeStore } from 'utils/store-dev';
 
-import { App } from 'containers/app';
+import { App } from 'containers/App';
 import { DEV as initialState } from 'fixtures/initial-state-dev.fixture';
 
 export function start(targetEl, payload) {
