51fd1afefdc5948f04f1f7a57ed052489766d6f4
Marco Pegoraro <marco.pegoraro@gmail.com>
2015-12-22 16:49:02 +0100

    update redux and redux-dev-tools

--- app/utils/store-dev.js
+++ app/utils/store-dev.js
@@ -6,19 +6,20 @@ import { reducers } from 'reducers';
 const reducer = combineReducers(reducers);
 const middlewares = [reduxThunk];
 
+import { DevTools } from './main-dev';
+
 var store;
 
 export function makeStore(initialState) {
 
     var finalCreateStore = compose(
         applyMiddleware(...middlewares),
-        require('redux-devtools').devTools(),
-        require('redux-devtools').persistState(
-          window.location.href.match(/[?&]debug_session=([^&]+)\b/)
-        )
+        DevTools.instrument(),
+        require('redux-devtools').persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
     )(createStore);
 
     store = finalCreateStore(reducer, initialState);
+
     return store;
 }
 
