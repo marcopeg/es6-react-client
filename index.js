a958d187bd458bc63f8c5252a4a786d603e92ac5
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-01-23 18:47:22 +0100

    server configuration #29

--- index.js
+++ index.js
@@ -2,30 +2,9 @@
  * Development Server
  */
 
-var HOST = '0.0.0.0';
-var PORT = 3000;
-
-var PROXY_ENABLED = true;
-var PROXY_HOST = HOST;
-var PROXY_PORT = PORT + 1;
-
-var PROXY_URLS = [
-    '/foo*',
-];
-
-var CUSTOM_PROXY_RULES = {
-    // '/marcopeg' : 'http://github.com/',
-};
-
-
-
-
-// ----------------------------------------------------------------------------- //
-// ---[[   D O   N O T   T O U C H   B E L O W   T H I S   C O M M E N T   ]]--- //
-// ----------------------------------------------------------------------------- //
-
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
+var serverCfg = require('./config/server.conf');
 
 // detect the request to run the styleguide
 var isStyleguide = process.argv.join(' ').indexOf('--guide') !== -1;
@@ -46,15 +25,15 @@ new WebpackDevServer(webpack(webpackConfig), {
     stats: {
         colors: true,
     },
-    proxy: proxyTable(PROXY_HOST, PROXY_PORT),
-}).listen(PORT, HOST, function (err) {
+    proxy: proxyTable(serverCfg.proxyHost, serverCfg.proxyPort),
+}).listen(serverCfg.port, serverCfg.host, function (err) {
     if (err) {
         console.log(err);
     }
 
-    console.log('Listening at localhost:' + PORT);
+    console.log('Listening at localhost:' + serverCfg.port);
 
-    if (PROXY_ENABLED && !isStyleguide) {
+    if (serverCfg.proxyIsEnabled && !isStyleguide) {
         runLocalAPI();
     }
 });
@@ -65,12 +44,12 @@ function proxyTable(host, port) {
         '/index.html' : proxyGuideEntryPoint(),
     };
 
-    PROXY_URLS.forEach(function (url) {
+    serverCfg.proxyUrls.forEach(function (url) {
         table[url] = 'http://' + host + ':' + port + '/';
     });
 
-    Object.keys(CUSTOM_PROXY_RULES).forEach(function (key) {
-        table[key] = CUSTOM_PROXY_RULES[key];
+    Object.keys(serverCfg.proxyRules).forEach(function (key) {
+        table[key] = serverCfg.proxyRules[key];
     });
 
     return table;
@@ -110,7 +89,11 @@ function runLocalAPI() {
         }
     );
 
-    app.listen((PROXY_PORT), function () {
-        console.log('Fake API /dist available at http://%s:%s', PROXY_HOST, PROXY_PORT);
+    app.listen((serverCfg.proxyPort), function () {
+        console.log(
+            'Fake API /dist available at http://%s:%s',
+            serverCfg.proxyHost,
+            serverCfg.proxyPort
+        );
     });
 }
